6fa4aa6a3220c377d0831402e23938aceefc53b5
Leo Okawa Ericson <git@relevant-information.com>
2022-06-22 16:59:44 +0200

    Implement first definitions

--- src/maxflowmincut.lean
+++ src/maxflowmincut.lean
@@ -1,2 +1,24 @@
+import data.real.basic
+import data.set
 import tactic
+open set
 
+open_locale big_operators
+universes u
+
+variable α : Type u
+
+local notation `V` := set α
+local notation `E` := set (α × α)
+
+class digraph :=
+  (nodes : V)
+  (edges : E)
+  (nonsymmetric : ∀ (u v : α ), (u, v) ∈ edges → (v, u) ∉ edges)
+
+@[class] structure flow_network
+  extends digraph α :=
+  (source : α)
+  (sink : α)
+  (capacity : E -> ℝ)
+  (postive_capacity : ∀ x : E, capacity x ≥ 0)
